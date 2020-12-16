9028e71ff1b1152c9c14070d93f46f30e9d4f4c8
maulanafajrin <lanafn201@gmail.com>
2020-12-16 16:58:40 +0800

    Init commite

---
+++ app/Models/Tanaman.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tanaman extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tanaman';
+
+    protected $primarykey ='id';
+
+    protected $fillable = 'nama';
+}
